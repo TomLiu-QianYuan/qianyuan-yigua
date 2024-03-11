f63b292e2b64cd1cf7afc1857471fb75650cbb2b
TomLiu <77840744+TomLiu-QianYuan@users.noreply.github.com>
2024-03-11 18:56:52 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -249,6 +249,8 @@ elif page == "书籍":
     list_files = os.listdir("书籍")
     file_to_read = st.selectbox("书籍选择", list_files)
     os.chdir("书籍")
+    st.text(os.getcwd())
+    st.text(os.listdir())
     if file_to_read in list_files:
         data = open(os.getcwd()+"\\书籍\\" + file_to_read, 'r', encoding='utf-8').read()
         # data_2 = ''
