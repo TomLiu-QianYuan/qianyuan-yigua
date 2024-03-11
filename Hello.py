a18c2af5fbca4c414d46f615e2fb9fd46508844e
TomLiu <77840744+TomLiu-QianYuan@users.noreply.github.com>
2024-03-11 13:57:07 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -240,14 +240,16 @@ if page == "起卦":
             # print(show)
         st.code("".join(show))
 elif page == "书籍":
+    if "书籍" in os.getcwd():
+        os.chdir("书籍")
     st.text(os.getcwd())
-    list_files = os.listdir(os.getcwd())
+    list_files = os.listdir()
     file_to_read = st.selectbox("书籍选择", list_files)
     st.code(list_files)
 
     if file_to_read in list_files:
         
-        data = open(os.getcwd()+"书籍\\"+file_to_read, 'r', encoding='utf-8').read()
+        data = open(file_to_read, 'r', encoding='utf-8').read()
         # data_2 = ''
         # for i in data.split('；'):
         #     data_2 += i + '；\n'
