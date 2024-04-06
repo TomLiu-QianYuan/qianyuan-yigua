d404bb500553e368d3b8a1c309a9ab27ea95d7ee
TomLiu <77840744+TomLiu-QianYuan@users.noreply.github.com>
2024-04-07 00:00:13 +0800

    Update 阅读.py

--- pages/阅读.py
+++ pages/阅读.py
@@ -1,6 +1,8 @@
 import os
 import streamlit as st
 import base64
+from pathlib import Path
+import tempfile
 
 if "pages" in os.getcwd():
     os.chdir("../")
@@ -9,30 +11,39 @@ if "书籍" in os.getcwd():
 print(os.getcwd())
 
 
-def displayPDF(file):
-    # Opening file from file path
-    with open(file, "rb", encoding='gbk') as f:
-        base64_pdf = base64.b64encode(f.read()).decode('gbk')
+def read_pdf(file_path):
+    # file = open(file_path, 'rb').read()
+    if file_path is not None:
+        with open(file_path, "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/pdf">'
+        st.markdown(pdf_display, unsafe_allow_html=True)
 
-    # Embedding PDF in HTML
-    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-
-    # Displaying File
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
+        # # print(file)
+        # with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        #     fp = Path(tmp_file.name)
+        #     fp.write_bytes(file)
+        #     with open(tmp_file.name, "rb") as f:
+        #         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        #     pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" ' \
+        #                   f'width="800" height="1000" type="application/pdf">'
+        #     st.markdown(pdf_display, unsafe_allow_html=True)
 
 list_files = os.listdir("书籍")
 file_to_read = st.selectbox("书籍选择", list_files)
 
 if file_to_read in list_files:
     os.chdir("书籍")
-    data = open(file_to_read, 'r', encoding='utf-8').read()
     # data_2 = ''
     # for i in data.split('；'):
     #     data_2 += i + '；\n'
-    os.chdir("../")
     if str(file_to_read).endswith(".txt"):
+        data = open(file_to_read, 'r', encoding='utf-8').read()
         st.code(data)
     elif str(file_to_read).endswith(".md"):
+        data = open(file_to_read, 'r', encoding='utf-8').read()
         st.markdown(data)
         # displayPDF(file_to_read)
+    elif str(file_to_read).endswith(".pdf"):
+        read_pdf(file_to_read)
+    os.chdir("../")
